287a765f018bc777f7473d45f832ec9d9e28396f
Vladimir Klepov <vklepov@ozon.ru>
2023-02-24 20:19:14 +0100

    minimal

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,29 @@
 type Cast<T, Source = unknown> = (data: Source) => T;
-export interface Banditype<T> extends Cast<T> {
+interface Banditype<T> extends Cast<T> {
   and<Extra = T>(cast: Cast<Extra, T>): Banditype<Extra>;
   or<Extra>(cast: Cast<Extra>): Banditype<T | Extra>;
 }
-export type Infer<Schema extends Cast<unknown>> = ReturnType<Schema>;
+type Infer<Schema extends Cast<unknown>> = ReturnType<Schema>;
 
 // factory
-export const banditype = <T>(cast: Cast<T>): Banditype<T> => {
-  const res: Banditype<T> = (raw: unknown) => cast(raw);
-  res.and = (extra) => banditype((raw) => extra(cast(raw)));
-  res.or = (extra) =>
+var banditype = <T>(cast: Cast<T>): Banditype<T> => ({
+  ...((raw) => cast(raw)),
+  and: (extra) => banditype((raw) => extra(cast(raw))),
+  or: (extra) =>
     banditype((raw) => {
       try {
         return cast(raw);
       } catch (err) {
         return extra(raw);
       }
-    });
-  return res;
-};
+    })
+}) as Banditype<T>;
 
 // error helper
-export const fail = (message?: string): never => {
-  throw new TypeError(message || "Banditype error");
-};
+var never = (message?: string): never => (null as any)[message || "Banditype error"] as never;
 
 // safe validation helper
-export const is = <T>(value: unknown, schema: Banditype<T>): value is T => {
+var is = <T>(value: unknown, schema: Banditype<T>): value is T => {
   try {
     schema(value);
     return true;
@@ -35,62 +32,68 @@ export const is = <T>(value: unknown, schema: Banditype<T>): value is T => {
   }
 };
 
-// end types
-export const never = () => banditype(fail);
-export const unknown = () => banditype((raw) => raw);
-
 // literals
-export const literal = <T>(item: T) =>
-  banditype((raw) => (Object.is(raw, item) ? (raw as T) : fail()));
-export const literalUnion = <T>(items: readonly T[]) =>
-  items.reduce((bt, item) => bt.or(literal(item)), never()) as Banditype<T>;
+var literalUnion = <T>(items: readonly T[]) => banditype(raw => items.includes(raw as T) ? raw as T : never());
 
-// empty
-export const btNull = () => banditype((raw) => (raw === null ? raw : fail()));
-export const btUndefined = () =>
-  banditype((raw) => (raw === undefined ? raw : fail()));
-
-// primitives
-export const string = () =>
-  banditype((raw) => (typeof raw === "string" ? raw : fail()));
-export const number = () =>
-  banditype((raw) => (typeof raw === "number" ? raw : fail()));
-export const boolean = () =>
-  banditype((raw) => (typeof raw === "boolean" ? raw : fail()));
-
-// function
-export const btFunction = () =>
-  banditype((raw) => (typeof raw === "function" ? raw : fail()));
+interface Like {
+  (tag: string): Banditype<string>;
+  (tag: number): Banditype<number>;
+  (tag: boolean): Banditype<boolean>;
+  (tag: (() => void)): Banditype<(...args: unknown[]) => unknown>;
+  (tag: object): Banditype<object | null>;
+}
+var like = ((tag: unknown) => banditype((raw) => (typeof raw === typeof tag ? raw : never()))) as Like;
 
 // objects
-export const anyObject = () =>
-  banditype((raw) =>
-    raw && typeof raw === "object" ? (raw as Record<string, unknown>) : fail()
-  );
-export const object = <T extends Record<string, Banditype<unknown>>>(
+var object = <T extends Record<string, Cast<unknown>>>(
   schema: T
 ) => {
-  return anyObject().and<{ [K in keyof T]: Infer<T[K]> }>((safe) => {
-    const res = {} as any;
-    for (const key in schema) {
-      res[key] = schema[key]!(safe[key]);
+  return banditype<{ [K in keyof T]: Infer<T[K]> }>((raw: any) => {
+    var res = {} as any;
+    for (var key in schema) {
+      res[key] = schema[key]!(raw[key]);
     }
     return res;
   });
 };
 
 // arrays
-export const anyArray = () =>
-  banditype<unknown[]>((raw) => (Array.isArray(raw) ? raw : fail()));
-export const array = <Item>(castItem: Banditype<Item>) =>
-  anyArray().and((arr) => arr.map(castItem));
-export const tuple = <T extends readonly Banditype<unknown>[]>(schema: T) =>
-  anyArray().and((arr) => {
+var array = <Item>(castItem: Cast<Item>) =>
+  instance(Array).and((arr) => arr.map(castItem));
+var tuple = <T extends readonly Cast<unknown>[]>(schema: T) =>
+  instance(Array).and((arr) => {
     return schema.map((cast, i) => cast(arr[i])) as {
-      [K in keyof T]: Infer<T[K]>;
+      -readonly [K in keyof T]: Infer<T[K]>;
     };
   });
 
 // classes
-export const instance = <T>(constructor: new () => T) =>
-  banditype((raw) => (raw instanceof constructor ? raw : fail()));
+var instance = <T>(constructor: new () => T) =>
+  banditype((raw) => (raw instanceof constructor ? raw as T : never()));
+
+export var m = object({
+  tags: array(like('')).and(t => t.length > 0),
+  coord: tuple([like(0), like(0)] as const),
+  created: instance(Date),
+  active: like(true),
+  close: like(like),
+  country: literalUnion(['EU', 'US'] as const).or(literalUnion([null] as const)),
+  tag: literalUnion(['HELLO'] as const).or(literalUnion([undefined] as const)),
+  extras: raw => raw,
+  bad: never,
+});
+
+// export var p = [
+//   object, 
+//   array, 
+//   like('').and(t => t.length > 0),
+//   tuple,
+//   like(0),
+//   instance(Date),
+//   like(true),
+//   like(()=>{}),
+//   literalUnion,
+//   literalUnion([null] as const),
+//   literalUnion([undefined] as const),
+//   raw => raw,
+// ];
