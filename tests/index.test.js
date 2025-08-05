0bcf8e9497a7b43a62e0dfddbb36f148717b99ff
vklepov <vklepov@ozon.ru>
2025-08-05 13:48:06 +0300

    node test runner

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,5 @@
-import { suite as makeSuite } from "uvu";
-import { equal, throws } from "uvu/assert";
+import { describe, test } from "node:test";
+import { deepEqual, throws } from "node:assert";
 import fs from "fs";
 import { dirname, resolve } from "path";
 import { fileURLToPath } from "url";
@@ -10,40 +10,28 @@ async function testValidation() {
   const kinds = fs.readdirSync(kindsDir).filter((t) => t[0] !== ".");
 
   for (const kind of kinds) {
-    const suite = makeSuite(kind);
+    describe(kind, async () => {
+      const testsDir = resolve(kindsDir, kind);
+      const tests = fs
+        .readdirSync(testsDir)
+        .filter((t) => t[0] !== "." && !t.endsWith(".d.ts"));
 
-    const testsDir = resolve(kindsDir, kind);
-    const tests = fs
-      .readdirSync(testsDir)
-      .filter((t) => t[0] !== "." && !t.endsWith(".d.ts"));
-
-    for (const name of tests) {
-      const module = await import(resolve(testsDir, name));
-      const { Struct, data, only, skip, output } = module;
-      const run = only ? suite.only : skip ? suite.skip : suite;
-      run(name, () => {
-        let actual;
-        let err;
-
-        if ("output" in module) {
-          equal(Struct(data), output);
-        } else if ("failures" in module) {
-          throws(() => Struct(data));
-          // const props = ['type', 'path', 'refinement', 'value', 'branch']
-          // const actualFailures = err
-          //   .failures()
-          //   .map((failure) => pick(failure, ...props))
-
-          // assert.deepStrictEqual(actualFailures, failures)
-          // assert.deepStrictEqual(pick(err, ...props), failures[0])
-        } else {
-          throw new Error(
-            `The "${name}" fixture did not define an \`output\` or \`failures\` export.`,
-          );
-        }
-      });
-    }
-    suite.run();
+      for (const name of tests) {
+        const module = await import(resolve(testsDir, name));
+        const { Struct, data, output } = module;
+        test(name, () => {
+          if ("output" in module) {
+            deepEqual(Struct(data), output);
+          } else if ("failures" in module) {
+            throws(() => Struct(data));
+          } else {
+            throw new Error(
+              `The "${name}" fixture did not define an \`output\` or \`failures\` export.`,
+            );
+          }
+        });
+      }
+    });
   }
 }
 
